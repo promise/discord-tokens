9c915e5e6829aaa83d4f8e85208caf3472b32859
Glenn <glenn@not-tech.support>
2023-02-27 08:55:51 +0000

    fix(lint): update and run lint
    Signed-off-by: Glenn <glenn@not-tech.support>

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { Octokit } = require("@octokit/rest");
 const { randomBytes } = require("crypto");
+const { Octokit } = require("@octokit/rest");
 
 /**
  * @typedef {Object} TokenResetOptions
